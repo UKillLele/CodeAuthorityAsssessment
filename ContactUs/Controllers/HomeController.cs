383f0caa884fc29303b35d6fad6e25c997559d70
UKillLele <mayerannam@gmail.com>
2019-08-12 16:14:04 -0500

    Add project files.

---
+++ ContactUs/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactUs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Questions? Comments? Let us know!";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstName, string lastName, string email, string telephone, string timeToCall)
+        {
+            ViewBag.Message = "Form submitted successfully - Thanks for contacting us!";
+            return View();
+        }
+    }
+}
---
+++ ContactUs/Models/contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ContactUs.Models
+{
+    public class contact
+    {
+        public string Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime timeToCall { get; set; }
+    }
+}
